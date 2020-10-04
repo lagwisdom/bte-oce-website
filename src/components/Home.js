003323444a6cc2aa12af4cb355fac5b6021a4d13
lagingwisdom <mrliamroliver@gmail.com>
2020-10-04 16:20:47 +0800

    test 4

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component{
             <div className="home_main">
                 <div className="landing_page">
                     <div className="title-text" >
-                        <h1 id="top_title">OCEANIA52</h1>
+                        <h1 id="top_title">OCEANIA</h1>
                         <hr id="title_divider"/>
                         <h3>Minecraft, 1:1 Scale</h3>
                     </div>
